5ff58f21fb1133e8bb584712f82b7cbccc4bd023
hu.wan <644986705@qq.com>
2019-04-28 15:00:11 +0800

    delete-src

--- src/model/Student.java
+++
@@ -1,8 +0,0 @@
-package model;
-
-/**
- * Created by  Hhhhh 2019-04-28 10:54
- */
-public class Student {
-
-}
--- src/stream/Calculate.java
+++
@@ -1,8 +0,0 @@
-package stream;
-
-/**
- * Created by Hhhhh 2019-04-28 11:08
- */
-public class Calculate {
-
-}
